42a2bb1ba64384c256891d24025134fd0e59c47e
ADHD is DEV <webtomekk@gmail.com>
2023-04-15 12:05:06 +0200

    migrations and models

--- ecom/database/migrations/2023_04_15_095439_create_products_table.php
+++ ecom/database/migrations/2023_04_15_095439_create_products_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('address');
-            $table->rememberToken();
+            $table->string('name', 255);
+            $table->string('slug', 255);
+            $table->text('description')->nullable();
+            $table->integer('price')->default(0);
+            $table->string('description')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
