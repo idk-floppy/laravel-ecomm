27103cf234e4a083d4f5b81023e409cd098cb723
Tamás Dávid Náhóczki <webtomekk@gmail.com>
2023-07-26 22:19:48 +0200

    Update 230726
    Promoted loading overlay to global component
    Made all components to global components, so they can be created in eachother, not only in parent app
    Edited views
    Reworked product form, so theres no duplicate for editing and creating
    Edited Cart vue
    Updated requests
    Updated product controller so it only gives back the products id on edit method

--- ecom/resources/js/components/services/SubmitEditProductForm.js
+++ ecom/resources/js/components/services/SubmitEditProductForm.js
@@ -1,5 +1,4 @@
 async function submitEditProductForm(formData, productId) {
-    formData.append('_method', 'PATCH')
     const response = await axios.post(`api/products/${productId}`, formData);
     return response.data;
 }
